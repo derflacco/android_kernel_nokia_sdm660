ba8671ccf9c24b0c6f89700dbc6c6ec2d5f9821b
Unknown <paul89rulez@gmail.com>
2020-03-25 12:21:00 +0100

    kernel\sched\core.c: fix merge derps

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -100,7 +100,6 @@
 
 ATOMIC_NOTIFIER_HEAD(load_alert_notifier_head);
 
-ATOMIC_NOTIFIER_HEAD(load_alert_notifier_head);
 
 DEFINE_MUTEX(sched_domains_mutex);
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
